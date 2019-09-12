d3660840668aa0f1c99a24633cc18ccdf09c7750
Youssef Victor <youssefvictor00@gmail.com>
2019-09-12 13:01:49 +0200

    Create Books view

---
+++ UBooks/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UBooks.Controllers
+{
+    [Authorize]
+    public class BooksController : Controller
+    {
+        // GET: Books/SellBook
+        public ActionResult SellBook()
+        {
+            return View();
+        }
+    }
+}
