3b592b2b939757bb4145eae068c8860631d07385
Youssef Victor <youssefvictor00@gmail.com>
2019-09-12 12:14:43 +0200

    Update book model

--- UBooks/Models/Book.cs
+++ UBooks/Models/Book.cs
@@ -7,9 +7,13 @@ namespace UBooks.Models
 {
     public class Book
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Author { get; set; }
         public DateTime PublishDate { get; set; }
+        public DateTime SellDate { get; set; }
+        public ApplicationUser Seller { get; set; }
+
     }
 }
