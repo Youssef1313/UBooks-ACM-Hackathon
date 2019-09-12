edb4c49b5b085cf039ccc54d1da11e31797933ef
Youssef Victor <youssefvictor00@gmail.com>
2019-09-12 15:32:13 +0200

    Implemented add and view a book for sell

--- UBooks/Controllers/BooksController.cs
+++ UBooks/Controllers/BooksController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using UBooks.Models;
 
 namespace UBooks.Controllers
@@ -21,9 +22,11 @@ namespace UBooks.Controllers
         [HttpPost]
         public async Task<ActionResult> Add(Book book)
         {
-            book.AddedDate = DateTime.Now;
+            string authorizedUserId = User.Identity.GetUserId();
             using (var context = new ApplicationDbContext())
             {
+                book.AddedDate = DateTime.Now;
+                book.AdvertisementOwner = context.Users.First(u => u.Id == authorizedUserId);
                 context.Books.Add(book);
                 await context.SaveChangesAsync();
             }
@@ -40,6 +43,24 @@ namespace UBooks.Controllers
                 return View(books);
             }
             
+        }
+
+        [AllowAnonymous]
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null) return HttpNotFound();
+            using (var context = new ApplicationDbContext())
+            {
+                var book = await context.Books.Include(b => b.AdvertisementOwner).FirstOrDefaultAsync(b => b.Id == id);
+                if (book == null) return HttpNotFound();
+
+                string authorizedUserId = User.Identity.GetUserId();
+                if (authorizedUserId == book.AdvertisementOwner.Id)
+                {
+                    // TODO: Redirect to edit book page.
+                }
+                return View("ReadOnlyBook", book);
+            }
         }
     }
 }
