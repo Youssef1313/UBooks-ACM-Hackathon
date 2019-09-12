4f2735469f44418945640d8f62f6a2f2c5f763e7
Youssef Victor <youssefvictor00@gmail.com>
2019-09-12 13:48:50 +0200

    Added form

--- UBooks/Controllers/BooksController.cs
+++ UBooks/Controllers/BooksController.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UBooks.Models;
 
 namespace UBooks.Controllers
 {
-    [Authorize]
+    // TODO: Remove comment [Authorize]
     public class BooksController : Controller
     {
         // GET: Books/SellBook
         public ActionResult SellBook()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Book book)
+        {
+            return HttpNotFound();
         }
     }
 }
--- UBooks/Models/Book.cs
+++ UBooks/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,20 @@ namespace UBooks.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Author { get; set; }
-        public DateTime PublishDate { get; set; }
-        public DateTime SellDate { get; set; }
-        public ApplicationUser SellerOrBuyer { get; set; }
-        public bool IsForSell { get; set; }
 
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        [Display(Name = "Published Date")]
+        public DateTime PublishDate { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        public ApplicationUser SellerOrBuyer { get; set; }
+
+        public bool IsForSell { get; set; }
     }
 }
