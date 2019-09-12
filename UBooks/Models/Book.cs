236ef0050a2711b49ae47c2ec6fa43a27bac2e4c
Youssef Victor <youssefvictor00@gmail.com>
2019-09-12 12:08:56 +0200

    Add book model

---
+++ UBooks/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UBooks.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
